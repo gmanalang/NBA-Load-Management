4d6d48d3c9b058782b62848baf606ac73251176f
vedagrawa <52718319+vedagrawa@users.noreply.github.com>
2023-10-23 20:27:23 -0400

    Add files via upload
    Imports the 2022-2033 data from pandas

---
+++ test_nba.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 23 20:18:05 2023
+
+@author: vedagrawal
+"""
+
+import pandas as pd
+
+df = pd.read_csv("NBAAverages2022-2023.csv")
+
+print(df)
+
