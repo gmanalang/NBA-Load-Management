c4d1eec7a15aa725d56a1ec326d3ca5b906bc971
Iggy Gonzales <147871303+iggygonzales@users.noreply.github.com>
2023-10-24 21:19:07 -0400

    Update test_nba.py

--- test_nba.py
+++ test_nba.py
@@ -7,9 +7,19 @@ Created on Mon Oct 23 20:18:05 2023
 test
 """
 
+# cell 1
 import pandas as pd
 
 df = pd.read_csv("NBAAverages2022-2023.csv")
 
-print(df)
+df
+
+# cell 2
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+sns.set()
+
+# plots relationship between points and mpg
+px.scatter(df, x='PTS', y='MP', hover_data = ['Player'], title = 'PTS vs MPG')
 
